ab7616a252b7935d8afbf8ef3801162b5682a916
nullzeal <nullzeal@pm.me>
2023-03-26 13:51:59 -0400

    Divided the solution into n-tier layers

---
+++ SpaceProgramApi/Controllers/EchoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaceProgramApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [Route("echo")]
+        [HttpGet]
+        public IActionResult Echo()
+        {
+            return Ok("Echo Gui " + DateTime.Now);
+        }
+    }
+}
