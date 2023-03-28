a2617c90d71c4d5fcb645e5d9d417469bc341d4a
nullzeal <nullzeal@pm.me>
2023-03-28 18:58:04 -0400

    Refactor of the officer create page

--- WebApp/Pages/Officer/Create.cshtml.cs
+++ WebApp/Pages/Officer/Create.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Text;
-using Newtonsoft.Json.Linq;
 using SpaceProgram.WebApp.DtoModels;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace WebApp.Pages.Officer;
 
@@ -11,61 +11,55 @@ public class CreateModel : PageModel
 {
     [BindProperty]
     public OfficerDto OfficerDto { get; set; }
-
-    private readonly HttpClient _httpClient;
+    public List<SpaceStationDto> StationsList { get; set; } = new List<SpaceStationDto>();
     public string Error { get; set; }
+    private readonly HttpClient _httpClient;
 
     public CreateModel(HttpClient httpClient)
     {
         _httpClient = httpClient;
-        StationsList = new List<SpaceStationDto>();
     }
-
-    public List<SpaceStationDto> StationsList { get; set; }
 
     public async Task OnGet()
     {
-        var response = await _httpClient.GetAsync("https://localhost:7202/api/spacestation");
-        var values = await response.Content.ReadAsStringAsync();
-        var obj = JObject.Parse(values);
-        foreach (var item in obj["fetchedSpaceStations"])
-        {
-            StationsList.Add(new SpaceStationDto((Guid)item["spaceStationId"], item["name"].ToString()));
-        }
+        await loadStationList();
     }
 
     public async Task<IActionResult> OnPost()
     {
-
-        var response1 = await _httpClient.GetAsync("https://localhost:7202/api/spacestation");
-        var values1 = await response1.Content.ReadAsStringAsync();
-        var obj1 = JObject.Parse(values1);
-        foreach (var item in obj1["fetchedSpaceStations"])
-        {
-            StationsList.Add(new SpaceStationDto((Guid)item["spaceStationId"], item["name"].ToString()));
-        }
+        await loadStationList();
 
         if (ModelState.IsValid)
         {
-            using StringContent jsonContent = new StringContent(
-                System.Text.Json.JsonSerializer.Serialize(OfficerDto),
-                Encoding.UTF8,
-                "application/json"
-            );
-
-            var response = await _httpClient.PostAsync("https://localhost:7202/api/officer/", jsonContent);
-            var values = await response.Content.ReadAsStringAsync();
-            var obj = JObject.Parse(values);
-
-            if (response.IsSuccessStatusCode)
+            try
             {
+                string officerString = JsonConvert.SerializeObject(OfficerDto, Formatting.None);
+                HttpContent officerContent = new StringContent(officerString, Encoding.UTF8, "application/json");
+                
+                var httpPostResponse = await _httpClient.PostAsync("https://localhost:7202/api/officer/", officerContent);
+                httpPostResponse.EnsureSuccessStatusCode();
+
                 return RedirectToPage("/Index");
             }
-            else
+            catch(Exception ex)
             {
-                Error = obj["detail"].ToString();
+                Error = ex.Message;
             }
         }
         return Page();
+    }
+
+    private async Task loadStationList()
+    {
+        var httpGetResponse = await _httpClient.GetAsync("https://localhost:7202/api/spacestation");
+        httpGetResponse.EnsureSuccessStatusCode();
+
+        var responseString = await httpGetResponse.Content.ReadAsStringAsync();
+        var jsonObject = JObject.Parse(responseString);
+
+        foreach (var fetchedSpaceStationString in jsonObject["fetchedSpaceStations"])
+        {
+            StationsList.Add(new SpaceStationDto((Guid)fetchedSpaceStationString["spaceStationId"], fetchedSpaceStationString["name"].ToString()));
+        }
     }
 }
