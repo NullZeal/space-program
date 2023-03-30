3385c407ea7642b10ee4be5294879ff17a24e71a
nullzeal <nullzeal@pm.me>
2023-03-30 15:22:02 -0400

    Add delete space station page Refactor of the officer delete page

--- WebApp/Pages/SpaceStation/Delete.cshtml.cs
+++ WebApp/Pages/SpaceStation/Delete.cshtml.cs
@@ -3,14 +3,14 @@ using WebApp.Business.DtoModels;
 using WebApp.Business.Extensions;
 using WebApp.Business.ParentPageModels;
 
-namespace WebApp.Pages.Officer
+namespace WebApp.Pages.SpaceStation
 {
     public class DeleteModel : LoginValidationModel
     {
         private readonly HttpClient _httpClient;
 
         [BindProperty]
-        public OfficerDto OfficerDto { get; set; } = new OfficerDto();
+        public SpaceStationDto SpaceStation { get; set; } = new SpaceStationDto();
         public string Error { get; set; }
 
         public DeleteModel(HttpClient httpClient)
@@ -23,20 +23,20 @@ namespace WebApp.Pages.Officer
             var validate = await base.OnGet(id);
             if (validate != null) { return validate; }
 
-            return await this.LoadOfficer(_httpClient, id, OfficerDto, Error);
+            return await this.LoadSpaceStation(_httpClient, id, SpaceStation, Error);
         }
 
         public async Task<IActionResult> OnPost(Guid id)
         {
-            var validate = await this.LoadOfficer(_httpClient, id, OfficerDto, Error);
+            var validate = await this.LoadSpaceStation(_httpClient, id, SpaceStation, Error);
             if (validate != null) { return validate; }
 
             try
             {
-                var httpPostResponse = await _httpClient.DeleteAsync($"https://localhost:7202/api/officer/{id}");
+                var httpPostResponse = await _httpClient.DeleteAsync($"https://localhost:7202/api/spacestation/{id}");
                 httpPostResponse.EnsureSuccessStatusCode();
 
-                return RedirectToPage("/officer/index");
+                return RedirectToPage("/spacestation/index");
             }
             catch (Exception ex)
             {
